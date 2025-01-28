74ec737483ba527732c8e67a6532fd9e84da0fa2
db-hasan <infoalihasanbd@gmail.com>
2025-01-28 12:43:05 +0600

    first commit

---
+++ resources/views/frontend/header.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Soft Query - Smart Software Solutions</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="{{ asset('backend/img/favicon.png') }}" rel="icon">
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('datatables/css/jquery.dataTables.css') }} " rel="stylesheet">
+    <link href="{{ asset('toastr/css/toastr.min.css') }} " rel="stylesheet">
+    <link href="{{ asset('backend/css/style.css') }}" rel="stylesheet">
+
+    <!-- +++++++++++++++++++++++++++++++++++++++++++
+        Design and Development by Ali Hasan
+        Contact: 01723629080
+        softqry.com
+        +++++++++++++++++++++++++++++++++++++++ -->
+</head>
